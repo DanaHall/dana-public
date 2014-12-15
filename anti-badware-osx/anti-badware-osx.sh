ffc278a09e984318a3f8ad35ea8ddc5fa4ef1e71
Brian Murphy <brian.murphy@danahall.org>
2014-12-15 12:48:50 -0500

    updated home address of nasties

--- anti-badware-osx/anti-badware-osx.sh
+++ anti-badware-osx/anti-badware-osx.sh
@@ -1,21 +1,60 @@
 #!bin/sh
 #summary script to remove all the nasty adware that has been making our lives difficult
+#some items outlines here are mentioned in Apple's Support site at :http://support.apple.com/en-us/HT6506
 
+#MacKeeper
 /Library/Application Support/MacKeeper
 /Library/LaunchDaemons/com.zeobit.MacKeeper.AntiVirus
 /Library/LaunchDaemons/com.zeobit.MacKeeper.plugin.AntiTheft.daemon
-
 Home/Library/Caches/com.zeobit.MacKeeper
-
 Home/Library/Caches/com.zeobit.MacKeeper.Helper
-
 Home/Library/LaunchAgents/com.zeobit.MacKeeper.Helper
-
 Home/Library/LaunchAgents/com.zeobit.MacKeeper.plugin.Backup.agent
-
 Home/Library/Preferences/com.zeobit.MacKeeper.plist
-
 Home/Library/Preferences/com.zeobit.MacKeeper.Helper.plist
-
 /Applications/MacKeeper
+
+#VSearch
+/Library/Application Support/VSearch
+/Library/LaunchAgents/com.vsearch.agent.plist
+/Library/LaunchDaemons/com.vsearch.daemon.plist
+/Library/LaunchDaemons/com.vsearch.helper.plist
+/Library/LaunchDaemons/Jack.plist
+/Library/PrivilegedHelperTools/Jack
+/System/Library/Frameworks/VSearch.framework
+
+#Trovi MyBrand Search Protect
+/Applications/SearchProtect.app
+/Library/LaunchAgents/com.conduit.loader.agent.plist
+/Library/LaunchDaemons/com.perion.searchprotectd.plst
+/Library/Application Support/SIMBL/Plugins/CT2285220.bundle
+~/Library/Internet Plug-Ins/ConduitNPAPIPlugin.plugin
+~/Library/Internet Plug-Ins/TroviNPAPIPlugin.plugin
+/Library/InputManagers/CTLoader/
+/Library/Application Support/Conduit/
+~/Conduit/
+~/Trovi/
+
+#Genieo InstallMac
+/Applications/Genieo
+/Applications/InstallMac
+/Applications/Uninstall Genieo
+/Applications/Uninstall IM Completer.app
+~/Library/LaunchAgents/com.genieo.completer.download.plist
+~/Library/LaunchAgents/com.genieo.completer.update.plist
+/Library/LaunchAgents/com.genieoinnovation.macextension.plist
+/Library/LaunchAgents/com.genieoinnovation.macextension.client.plist
+/Library/LaunchAgents/com.genieo.engine.plist
+/Library/LaunchAgents/com.genieo.completer.update.plist
+/Library/LaunchDaemons/com.genieoinnovation.macextension.client.plist
+/Library/PrivilegedHelperTools/com.genieoinnovation.macextension.client
+/usr/lib/libgenkit.dylib
+/usr/lib/libgenkitsa.dylib
+/usr/lib/libimckit.dylib
+/usr/lib/libimckitsa.dylib
+~/Preferences/com.apple.genieo.global.settings.plist
+~/SavedState/com.genie.RemoveGenieoMac.savedState
+~/Library/Application Support/Genieo/
+~/Library/Application Support/com.genieoinnovation.Installer/
+/Library/Frameworks/GenieoExtra.framework
 
