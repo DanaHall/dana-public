b85637eba4c5a77a18ea3540534b4283130533d7
Brian Murphy <brian.murphy@danahall.org>
2014-12-15 12:36:56 -0500

    added anti-badware-osx

---
+++ anti-badware-osx/anti-badware-osx.sh
@@ -0,0 +1,21 @@
+#!bin/sh
+#summary script to remove all the nasty adware that has been making our lives difficult
+
+/Library/Application Support/MacKeeper
+/Library/LaunchDaemons/com.zeobit.MacKeeper.AntiVirus
+/Library/LaunchDaemons/com.zeobit.MacKeeper.plugin.AntiTheft.daemon
+
+Home/Library/Caches/com.zeobit.MacKeeper
+
+Home/Library/Caches/com.zeobit.MacKeeper.Helper
+
+Home/Library/LaunchAgents/com.zeobit.MacKeeper.Helper
+
+Home/Library/LaunchAgents/com.zeobit.MacKeeper.plugin.Backup.agent
+
+Home/Library/Preferences/com.zeobit.MacKeeper.plist
+
+Home/Library/Preferences/com.zeobit.MacKeeper.Helper.plist
+
+/Applications/MacKeeper
+
